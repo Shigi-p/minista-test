7cc95c036de9fb7e12129ebc63f733a110895bf7
Shigi-p <shigi.522@gmail.com>
2023-06-02 18:19:18 +0900

    build(minista.config.ts): add image optimize settings

--- minista.config.ts
+++ minista.config.ts
@@ -2,8 +2,18 @@ import path from "path"
 import { defineConfig } from "minista"
 
 export default defineConfig({
-  base: "./",
   resolve: {
     alias: [{ find: "@/", replacement: path.resolve("src") + "/" }],
+  },
+  assets: {
+    images: {
+      // なぜかここの部分を実装のパスと一緒にしないとエラーが出る
+      // これにすると書き出しがdist/src/assets/imagesになってしまうので、画像の格納場所をassets/imagesに合わせるように調整
+      // outDir: "src/assets/images",
+      optimize: {
+        breakpoints: [320, 1024, 1920, 2560],
+        format: "webp",
+      },
+    },
   },
 })
